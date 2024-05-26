5456d0fb2cd7cc1965ff1203bc6710a6059484cc
KutsunaSubaRya <index20010928@gmail.com>
2024-05-27 05:31:18 +0800

    fix: fix sound not sync with view

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 import math
+import threading
 
-pygame.mixer.pre_init(22050, -16, 2, 1024)
 pygame.init()
-pygame.mixer.quit()
-pygame.mixer.init(22050, -16, 2, 1024)
+pygame.mixer.init()
+
+# Load sound files
+pygame.mixer.music.load('output.mid')
 
 WIDTH, HEIGHT = 1000, 800
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Three Body Problem Simulation")
+blueBlackLock = False
+greenBlackLock = False
+redBlackLock = False
 
-pygame.mixer.music.load('output.mid')
+
+def play_sound():
+    threading.Thread(target=pygame.mixer.music.play()).start()
+
+
 
 class Body:
     def __init__(self, x, y, vx, vy, mass, color, size):
@@ -28,16 +37,14 @@ class Body:
             if body != self:
                 dx = body.x - self.x
                 dy = body.y - self.y
-                distance = math.sqrt(dx**2 + dy**2)
+                distance = math.sqrt(dx ** 2 + dy ** 2)
                 if distance < 30:
                     distance = 30
-                force = self.mass * body.mass / distance**2
+                force = self.mass * body.mass / distance ** 2
                 angle = math.atan2(dy, dx)
                 self.vx += force * math.cos(angle) / self.mass * dt
                 self.vy += force * math.sin(angle) / self.mass * dt
-                if body == bodies[3] and distance < 100 and distance > 99:
-                    pygame.mixer.music.play()
-                    print("OK")
+
     def update_position(self, dt):
         self.x += self.vx * dt
         self.y += self.vy * dt
@@ -45,14 +52,22 @@ class Body:
 
     def draw(self):
         pygame.draw.circle(win, self.color, (int(self.x), int(self.y)), self.size)
-    
+
     def drawpath(self):
         for i in range(len(self.path) - 1):
             pygame.draw.line(win, self.color, self.path[i], self.path[i + 1], 1)
 
-body1 = Body(WIDTH // 2 - 59.1, HEIGHT // 2,                        0, 0,    1,     (0, 0, 255), 10)
-body2 = Body(WIDTH // 2 + 61.1, HEIGHT // 2,                        0, 0,    1,     (0, 255, 0), 10)
-body3 = Body(WIDTH // 2, HEIGHT // 2 + 61 * math.sqrt(3),         0, 0,    1,     (255, 0, 0), 10)
+
+def check_body_distance(b1: Body, b2: Body):
+    dx = b2.x - b1.x
+    dy = b2.y - b1.y
+    distance = math.sqrt(dx ** 2 + dy ** 2)
+    return distance
+
+
+body1 = Body(WIDTH // 2 - 59.1, HEIGHT // 2, 0, 0, 1, (0, 0, 255), 10)
+body2 = Body(WIDTH // 2 + 61.1, HEIGHT // 2, 0, 0, 1, (0, 255, 0), 10)
+body3 = Body(WIDTH // 2, HEIGHT // 2 + 61 * math.sqrt(3), 0, 0, 1, (255, 0, 0), 10)
 body4 = Body(WIDTH // 2, HEIGHT // 2 + 60 * math.sqrt(3) + 31.1, 0.05, 0, 0.03, (100, 100, 100), 1)
 
 bodies = [body1, body2, body3, body4]
@@ -64,23 +79,42 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     for body in bodies:
         body.update_velocity(bodies, dt)
-
     for body in bodies:
         body.update_position(dt)
+    if check_body_distance(body1, body4) < 20 and blueBlackLock: # Blue Black
+        print("Blue Black")
+        pygame.mixer.music.play()
+        body1.color = (0, 0, 0)
+        blueBlackLock = False
+    elif check_body_distance(body1, body4) >= 20:
+        body1.color = (0, 0, 255)
+        blueBlackLock = True
+    if check_body_distance(body2, body4) < 20 and greenBlackLock: # Green Black
+        print("Green Black")
+        pygame.mixer.music.play()
+        body2.color = (0, 0, 0)
+        greenBlackLock = False
+    elif check_body_distance(body2, body4) >= 20:
+        greenBlackLock = True
+        body2.color = (0, 255, 0)
+    if check_body_distance(body3, body4) < 20 and redBlackLock: # Red Black
+        print("Red Black")
+        pygame.mixer.music.play()
+        body3.color = (0, 0, 0)
+        redBlackLock = False
+    elif check_body_distance(body3, body4) >= 20:
+        redBlackLock = True
+        body3.color = (255, 0, 0)
 
     win.fill((200, 200, 200))
-
-    #for effect in EffectList:
-        #effect.draw()
 
     bodies[3].drawpath()
 
     for body in bodies:
         body.draw()
-    
+
     pygame.display.update()
     clock.tick(60)
 
